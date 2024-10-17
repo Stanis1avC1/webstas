ab46ba8d0eb9917ae6c7a300bb20f4d9d077d93c
Stanislav Chobal <chobal.stanislav@student.uzhnu.edu.ua>
2024-10-17 17:23:30 +0300

    test

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,15 +4,14 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(); // Виправлено на undefined
-
+    const [user, setUser] = useState();
     const login = (userData) => {
         setUser(userData);
         localStorage.setItem('user', JSON.stringify(userData));
     };
 
     const logout = () => {
-        setUser(undefined); // Виправлено на undefined
+        setUser(undefined);
         localStorage.removeItem('user');
     };
 
